ccce4fe9629096766f966fd789474ad9f2782f32
WangYiChuan <wangyichuan@finlean.com>
2018-10-19 18:13:03 +0800

    code
    Signed-off-by: WangYiChuan <wangyichuan@finlean.com>

--- BotDemo.py
+++ BotDemo.py
@@ -4,17 +4,21 @@ from qqbot import QQBotSlot as qqbotslot, RunBot
 
 @qqbotslot
 def onQQMessage(bot, contact, member, content):
-    if content == '-hello':
-
-        bot.SendTo(contact, '你好，我是QQ机器人')
-    elif content == '在':
-        bot.SendTo(contact, '我一直在呢，嘻嘻')
-
-
-    elif content == '-stop':
-
-        bot.SendTo(contact, 'QQ机器人已关闭')
+    # print(bot)
+    # print(contact)
+    # print(member)
+    # print(content)
+    # if contact == '群\"botTest\"':
+    if content == '机器人在不在':
+        bot.SendTo(contact, '在的 在的')
+    elif content == '机器人阔步阔耐':
+        bot.SendTo(contact, '非常阔耐')
+    elif content == '你走 我不要你了':
+        bot.SendTo(contact, '机器人走了,并关上了门')
         bot.Stop()
+    # elif contact == '好友\"、Joker\"':
+    #     bot.SendTo('群\"botTest\"','来自、Joker的消息:'+contact)
+    #     pass
 
 
 if __name__ == '__main__':
